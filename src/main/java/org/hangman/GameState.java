bb07c8521ce01539126f8277ee2e509e85517cbe
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-24 15:57:24 +0000

    Added method to generate a hidden word

--- src/main/java/org/hangman/GameState.java
+++ src/main/java/org/hangman/GameState.java
@@ -1,4 +1,30 @@
 package org.hangman;
 
 public class GameState {
+
+    private String currentWord;
+    private String hiddenWord;
+
+    public GameState(String currentWord, String hiddenWord) {
+        this.currentWord = currentWord;
+        this.hiddenWord = hiddenWord;
+    }
+
+    /**
+     * Generates a hidden version of the current word with underscores
+     * to represent hidden letters, separated by spaces.
+     *
+     * @param word The current word.
+     * @return A string with underscores to represent hidden letters.
+     */
+    private String generateHiddenWord(String word) {
+        StringBuilder hiddenWord = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            hiddenWord.append("_");
+            if (i < word.length() - 1) {
+                hiddenWord.append(" ");
+            }
+        }
+        return hiddenWord.toString();
+    }
 }
