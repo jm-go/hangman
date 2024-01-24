e716f68c8b855b6f99861643ee4729cb3810984b
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-24 15:19:32 +0000

    Updated Word class

--- src/main/java/org/hangman/Word.java
+++ src/main/java/org/hangman/Word.java
@@ -1,4 +1,25 @@
 package org.hangman;
 
 public class Word {
+
+    private static final String[] regularLevel = {
+            "Balloon", "Elephant", "Flamingo",
+            "Glitter", "Harmony", "Infinite",
+            "Jigsaw", "Keyboard", "Lantern",
+            "Mountain", "Necklace", "Octagon",
+            "Painting", "Question", "Rainbow",
+            "Sunlight", "Treasure", "Umbrella",
+            "Volcano", "Whistle"
+    };
+
+    private static final String[] advancedLevel = {
+            "Acceleration", "Biotechnology", "Consequential",
+            "Encapsulation", "Fingerprint", "Gravitational",
+            "Illumination", "Kaleidoscope", "Ornithology",
+            "Photosynthesis", "Questionnaire", "Synchronization",
+            "Boulevard", "Silhouette", "Misinterpretation",
+            "Unpredictable", "Inconsequential", "Disproportionate",
+            "Anthropologist", "Philosophical"
+    };
+
 }
